fec86188fa2c1fc7eccd198b0b1ad10fcdc9d559
anthonyray <dantho361@hotmail.com>
2014-07-09 10:40:21 -0700

    Fixed the synchronous read of the example resume.json file

--- server.js
+++ server.js
@@ -31,89 +31,102 @@ function runTheme(options, req, res) {
 
 var getTheme = function(req, res) {
 
-  var resume = JSON.parse(fs.readFileSync('resume.json', 'utf8')); // Be careful, synchronous operation can block the event loop. 
+  fs.readFile('resume.json', 'utf8',function(err,data){
+    if (err) res.send("Server error!")
+    else {
+      var resume = JSON.parse(data);
 
-  if(req.body && req.body.resume) {
-    console.log('USE POSTED RESUME');
-    
-    resume = req.body.resume;
-  }
-  var theme = 'jsonresume-theme-' + req.params.theme;
-  var version = '0';
-  var versionCheck = theme.split('@');
-  if (versionCheck.length > 1) {
-    theme = versionCheck[0];
-    version = versionCheck[1];
-  }
+      if(req.body && req.body.resume) {
+        console.log('USE POSTED RESUME');
+        
+        resume = req.body.resume;
+      }
+      
+      var theme = 'jsonresume-theme-' + req.params.theme;
+      var version = '0';
+      var versionCheck = theme.split('@');
+      if (versionCheck.length > 1) {
+        theme = versionCheck[0];
+        version = versionCheck[1];
+      }
 
-  var directoryFolder = path.join(themeDir, theme, version);
+      var directoryFolder = path.join(themeDir, theme, version);
 
-  console.log(theme, version);
-  console.log('why ont execute');
-  fs.exists(directoryFolder, function(exists) {
-    console.log(directoryFolder, exists);
-    if (exists && version !== '0') {
-      console.log('Theme cached');
-      runTheme({
-        themeDirectory: directoryFolder,
-        resume: resume
-      }, req, res);
-      return;
-    } else {
-      console.log('Chceking NPM');
-      request.get('https://registry.npmjs.org/' + theme, function(response) {
+      console.log(theme, version);
+      console.log('why ont execute');
+      fs.exists(directoryFolder, function(exists) {
+        console.log(directoryFolder, exists);
+        if (exists && version !== '0') {
+          console.log('Theme cached');
+          runTheme({
+            themeDirectory: directoryFolder,
+            resume: resume
+          }, req, res);
+          return;
+        } else {
+          console.log('Chceking NPM');
+          request.get('https://registry.npmjs.org/' + theme, function(response) {
 
-        var lib = response.body;
-        if (version === '0') {
-          version = lib['dist-tags'].latest;
+            var lib = response.body;
+            if (version === '0') {
+              version = lib['dist-tags'].latest;
+            }
+
+            var directoryFolder = path.join(themeDir, theme, version);
+            fs.exists(directoryFolder, function(exists) {
+              if (exists) {
+                runTheme({
+                  themeDirectory: directoryFolder,
+                  resume: resume
+                }, req, res);
+                return;
+              } else {
+                console.log(directoryFolder);
+                var tarballURL = lib.versions[version].dist.tarball;
+                console.log(tarballURL);
+                var themeVersion = theme + '@' + version;
+
+                mkdirp(directoryFolder, function(err) {
+                  var tempExtractPath = __dirname + '/tmp/' + themeVersion;
+                  console.log('Downloading NPM module');
+                  tarball.extractTarballDownload(tarballURL, __dirname + '/tmp/' + themeVersion + '.tar.gz', tempExtractPath, {}, function(err, result) {
+                    fs.readdir(tempExtractPath, function(err, files) {
+                      var containingFolder = files[0];
+                      fs.rename(path.join(tempExtractPath, containingFolder), directoryFolder, function() {
+                        console.log('Installing dependencies');
+                        child = exec('cd ' + directoryFolder + ' && npm install',
+                          function(error, stdout, stderr) {
+
+                            runTheme({
+                              themeDirectory: directoryFolder,
+                              resume: resume
+                            }, req, res);
+                            if (error !== null) {
+                              console.log('exec error: ' + error);
+                            }
+                          });
+                      });
+                    })
+                  })
+                });
+              }
+            });
+
+          });
+          // Do something
         }
 
-        var directoryFolder = path.join(themeDir, theme, version);
-        fs.exists(directoryFolder, function(exists) {
-          if (exists) {
-            runTheme({
-              themeDirectory: directoryFolder,
-              resume: resume
-            }, req, res);
-            return;
-          } else {
-            console.log(directoryFolder);
-            var tarballURL = lib.versions[version].dist.tarball;
-            console.log(tarballURL);
-            var themeVersion = theme + '@' + version;
-
-            mkdirp(directoryFolder, function(err) {
-              var tempExtractPath = __dirname + '/tmp/' + themeVersion;
-              console.log('Downloading NPM module');
-              tarball.extractTarballDownload(tarballURL, __dirname + '/tmp/' + themeVersion + '.tar.gz', tempExtractPath, {}, function(err, result) {
-                fs.readdir(tempExtractPath, function(err, files) {
-                  var containingFolder = files[0];
-                  fs.rename(path.join(tempExtractPath, containingFolder), directoryFolder, function() {
-                    console.log('Installing dependencies');
-                    child = exec('cd ' + directoryFolder + ' && npm install',
-                      function(error, stdout, stderr) {
-
-                        runTheme({
-                          themeDirectory: directoryFolder,
-                          resume: resume
-                        }, req, res);
-                        if (error !== null) {
-                          console.log('exec error: ' + error);
-                        }
-                      });
-                  });
-                })
-              })
-            });
-          }
-        });
-
       });
-      // Do something
+
+
     }
 
   });
-}
+
+  var resume = JSON.parse(fs.readFileSync('resume.json', 'utf8')); // Be careful, synchronous operation can block the event loop. 
+
+};
+
 app.post('/theme/:theme', getTheme);
 
 app.get('/theme/:theme', getTheme);
