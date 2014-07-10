6db89b3ab1fedae9924c463fc3bf6477f87833a0
Mattias Erming <mattias@mattiaserming.com>
2014-07-10 02:19:48 +0200

    Use resume.json from resume-schema

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ var app = express();
 
 app.use(bodyParser.json())
 
+var resume = require('resume-schema').resumeJson;
 var themeDir = 'themes';
 
 
@@ -30,95 +31,82 @@ function runTheme(options, req, res) {
 };
 
 var getTheme = function(req, res) {
+  if (req.body && req.body.resume) {
+    console.log('Use posted resume');
+    resume = req.body.resume;
+  }
+  
+  var theme = 'jsonresume-theme-' + req.params.theme;
+  var version = '0';
+  var versionCheck = theme.split('@');
+  if (versionCheck.length > 1) {
+    theme = versionCheck[0];
+    version = versionCheck[1];
+  }
 
-  fs.readFile('resume.json', 'utf8',function(err,data){
-    if (err) res.send("Server error!")
-    else {
-      var resume = JSON.parse(data);
+  var directoryFolder = path.join(themeDir, theme, version);
 
-      if(req.body && req.body.resume) {
-        console.log('USE POSTED RESUME');
-        
-        resume = req.body.resume;
-      }
-      
-      var theme = 'jsonresume-theme-' + req.params.theme;
-      var version = '0';
-      var versionCheck = theme.split('@');
-      if (versionCheck.length > 1) {
-        theme = versionCheck[0];
-        version = versionCheck[1];
-      }
+  console.log(theme, version);
+  fs.exists(directoryFolder, function(exists) {
+    console.log(directoryFolder, exists);
+    if (exists && version !== '0') {
+      console.log('Theme cached');
+      runTheme({
+        themeDirectory: directoryFolder,
+        resume: resume
+      }, req, res);
+      return;
+    } else {
+      console.log('Checking NPM');
+      request.get('https://registry.npmjs.org/' + theme, function(response) {
 
-      var directoryFolder = path.join(themeDir, theme, version);
-
-      console.log(theme, version);
-      //console.log('why ont execute');
-      fs.exists(directoryFolder, function(exists) {
-        console.log(directoryFolder, exists);
-        if (exists && version !== '0') {
-          console.log('Theme cached');
-          runTheme({
-            themeDirectory: directoryFolder,
-            resume: resume
-          }, req, res);
-          return;
-        } else {
-          console.log('Chceking NPM');
-          request.get('https://registry.npmjs.org/' + theme, function(response) {
-
-            var lib = response.body;
-            if (version === '0') {
-              version = lib['dist-tags'].latest;
-            }
-
-            var directoryFolder = path.join(themeDir, theme, version);
-            fs.exists(directoryFolder, function(exists) {
-              if (exists) {
-                runTheme({
-                  themeDirectory: directoryFolder,
-                  resume: resume
-                }, req, res);
-                return;
-              } else {
-                console.log(directoryFolder);
-                var tarballURL = lib.versions[version].dist.tarball;
-                console.log(tarballURL);
-                var themeVersion = theme + '@' + version;
-
-                mkdirp(directoryFolder, function(err) {
-                  var tempExtractPath = __dirname + '/tmp/' + themeVersion;
-                  console.log('Downloading NPM module');
-                  tarball.extractTarballDownload(tarballURL, __dirname + '/tmp/' + themeVersion + '.tar.gz', tempExtractPath, {}, function(err, result) {
-                    fs.readdir(tempExtractPath, function(err, files) {
-                      var containingFolder = files[0];
-                      fs.rename(path.join(tempExtractPath, containingFolder), directoryFolder, function() {
-                        console.log('Installing dependencies');
-                        child = exec('cd ' + directoryFolder + ' && npm install',
-                          function(error, stdout, stderr) {
-
-                            runTheme({
-                              themeDirectory: directoryFolder,
-                              resume: resume
-                            }, req, res);
-                            if (error !== null) {
-                              console.log('exec error: ' + error);
-                            }
-                          });
-                      });
-                    })
-                  })
-                });
-              }
-            });
-
-          });
-          // Do something
+        var lib = response.body;
+        if (version === '0') {
+          version = lib['dist-tags'].latest;
         }
 
+        var directoryFolder = path.join(themeDir, theme, version);
+        fs.exists(directoryFolder, function(exists) {
+          if (exists) {
+            runTheme({
+              themeDirectory: directoryFolder,
+              resume: resume
+            }, req, res);
+            return;
+          } else {
+            console.log(directoryFolder);
+            var tarballURL = lib.versions[version].dist.tarball;
+            console.log(tarballURL);
+            var themeVersion = theme + '@' + version;
+
+            mkdirp(directoryFolder, function(err) {
+              var tempExtractPath = __dirname + '/tmp/' + themeVersion;
+              console.log('Downloading NPM module');
+              tarball.extractTarballDownload(tarballURL, __dirname + '/tmp/' + themeVersion + '.tar.gz', tempExtractPath, {}, function(err, result) {
+                fs.readdir(tempExtractPath, function(err, files) {
+                  var containingFolder = files[0];
+                  fs.rename(path.join(tempExtractPath, containingFolder), directoryFolder, function() {
+                    console.log('Installing dependencies');
+                    child = exec('cd ' + directoryFolder + ' && npm install',
+                      function(error, stdout, stderr) {
+
+                        runTheme({
+                          themeDirectory: directoryFolder,
+                          resume: resume
+                        }, req, res);
+                        if (error !== null) {
+                          console.log('exec error: ' + error);
+                        }
+                      });
+                  });
+                })
+              })
+            });
+          }
+        });
+
       });
-
-
+      // Do something
     }
 
   });
