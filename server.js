2a35e30da93fd02e50655464a663d84838108bd8
Mattias Erming <mattias@mattiaserming.com>
2014-07-16 21:07:09 -0700

    Added /themes.json

--- server.js
+++ server.js
@@ -8,10 +8,13 @@ var app = express();
 app.use(bodyParser.json())
 app.use(cors());
 
-app.all('/', themes);
-app.all('/themes', themes);
-app.all('/:theme', theme);
-app.all('/theme/:theme', theme);
+app.get("/themes.json", themes);
+app.get('/:theme', theme);
+app.post('/theme/:theme', theme);
+
+app.get('/theme/:theme', theme);
+app.post('/theme/:theme', theme);
+
 app.listen(3000);
 
 console.log('Server running at http://localhost:3000/');
